a6aec35a7bfb3784eef23afd52044aeb942b30a9
GouthamGuna <ggowthamsankar@gmail.com>
2022-11-17 11:08:58 +0530

    learn and add slf4j.Logger

--- src/main/java/in/gmsk/springbootapp/controller/LoginController.java
+++ src/main/java/in/gmsk/springbootapp/controller/LoginController.java
@@ -1,5 +1,7 @@
 package in.gmsk.springbootapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller("/")
 public class LoginController {
 
+	private Logger logger=LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping
 	public String goToLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
+		logger.debug("Request Param {} ",name);
 		return "login";
 	}
 }
