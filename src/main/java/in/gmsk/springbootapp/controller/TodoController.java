d43ad972b039919ce298cc827ad0429d7e556c3f
GouthamGuna <ggowthamsankar@gmail.com>
2022-11-22 10:17:33 +0530

    create the get input method and save to static list

--- src/main/java/in/gmsk/springbootapp/controller/TodoController.java
+++ src/main/java/in/gmsk/springbootapp/controller/TodoController.java
@@ -1,10 +1,13 @@
 package in.gmsk.springbootapp.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import in.gmsk.springbootapp.entity.ToDoPojo;
@@ -31,9 +34,18 @@ public class TodoController {
 		return "listToDos";
 	}
 	
-	@RequestMapping("/mylist/add-todo")
+	@RequestMapping(value = "/add-todo", method = RequestMethod.GET)
 	public String showAddToDoPage(){
 		
 		return "addToDoPage";
+	}
+	
+	@RequestMapping(value = "/add-todo", method = RequestMethod.POST)
+	public String AddToDoPage(@RequestParam String description, ModelMap model){
+		
+		String username=(String)model.get("username");
+		doService.addToDo(username, description, LocalDate.now(), false);
+		
+		return "redirect:mylist";
 	}
 }
--- src/main/java/in/gmsk/springbootapp/service/ToDoService.java
+++ src/main/java/in/gmsk/springbootapp/service/ToDoService.java
@@ -13,13 +13,21 @@ public class ToDoService {
 
 	private static List<ToDoPojo> retrieve=new ArrayList<>();
 	
+	private static int todosCount = 0;
+	
 	static {
-		retrieve.add(new ToDoPojo(1, "Gowtham", "Learn AWS", LocalDate.now().plusYears(1), false));
-		retrieve.add(new ToDoPojo(2, "Jay kumar", "Learn AWS", LocalDate.now().plusYears(1), false));
-		retrieve.add(new ToDoPojo(3, "Karthik", "Learn AWS", LocalDate.now().plusYears(1), false));
+		retrieve.add(new ToDoPojo(++todosCount, "Gowtham", "Learn AWS", LocalDate.now().plusYears(1), false));
+		retrieve.add(new ToDoPojo(++todosCount, "Jay kumar", "Learn AWS", LocalDate.now().plusYears(1), false));
+		retrieve.add(new ToDoPojo(++todosCount, "Karthik", "Learn AWS", LocalDate.now().plusYears(1), false));
 	}
 	
 	public List<ToDoPojo> findByUserName(String userName){
 		return retrieve;
+	}
+	
+	public void addToDo(String username, String description, LocalDate date, boolean isDone) {
+		
+		ToDoPojo obj=new ToDoPojo(++todosCount, username, description, date, isDone);
+		retrieve.add(obj);
 	}
 }
