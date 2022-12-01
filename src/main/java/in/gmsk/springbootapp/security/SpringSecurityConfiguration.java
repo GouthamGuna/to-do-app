12f0c40e25fbe30aad08727e848f600907504b88
GouthamGuna <ggowthamsankar@gmail.com>
2022-12-01 22:19:01 +0530

    newly learn passwordEncoder and implement the application

--- src/main/java/in/gmsk/springbootapp/security/SpringSecurityConfiguration.java
+++ src/main/java/in/gmsk/springbootapp/security/SpringSecurityConfiguration.java
@@ -1,9 +1,13 @@
 package in.gmsk.springbootapp.security;
 
+import java.util.function.Function;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -15,12 +19,21 @@ public class SpringSecurityConfiguration {
 	
 	@Bean
 	public InMemoryUserDetailsManager createUser() {
-		UserDetails user = User.withDefaultPasswordEncoder()
-		.username("gowtham")
-		.password("sreeja")
-		.roles("ADMIN","USER")
-		.build();
+		
+		Function<String, String> encoder = input -> encoder().encode(input);
+		
+		UserDetails user = User.builder()
+								.passwordEncoder(encoder)
+								.username("gowtham")
+								.password("sreeja")
+								.roles("ADMIN","USER")
+								.build();
 		return new InMemoryUserDetailsManager(user);
+	}
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
