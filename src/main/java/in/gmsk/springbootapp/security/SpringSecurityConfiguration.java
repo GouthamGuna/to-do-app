977aed368791f29f4f81cd285702252028a8df5b
GouthamGuna <ggowthamsankar@gmail.com>
2022-12-09 16:25:32 +0530

    learn H2 database connection and add the data in H2 database

--- src/main/java/in/gmsk/springbootapp/security/SpringSecurityConfiguration.java
+++ src/main/java/in/gmsk/springbootapp/security/SpringSecurityConfiguration.java
@@ -1,14 +1,18 @@
 package in.gmsk.springbootapp.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -43,4 +47,23 @@ public class SpringSecurityConfiguration {
 		return new BCryptPasswordEncoder();
 	}
 
+	/*
+	 * All URL's are protected
+	 * A Login from is shown for unauthorized requests
+	 * CSRF disable
+	 * Frames are allow
+	 * 
+	 */
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+		
+		http.formLogin(withDefaults());
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		
+		return http.build();
+	}
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into TO_DO_POJO (ID, USER_NAME, DESCRIPTION, DATE, DONE) values(1001, 'Gowtham Sankar', 'Learn Azure Developer! DP100', CURRENT_DATE(), false);
+
+insert into TO_DO_POJO (ID, USER_NAME, DESCRIPTION, DATE, DONE) values(1002, 'Sreeja', 'Learn Azure Developer!', CURRENT_DATE(), false);
+
+insert into TO_DO_POJO (ID, USER_NAME, DESCRIPTION, DATE, DONE) values(1003, 'Jay Kumar', 'Learn Azure Developer!', CURRENT_DATE(), false);
+
+insert into TO_DO_POJO (ID, USER_NAME, DESCRIPTION, DATE, DONE) values(1004, 'Manoj', 'Learn AWS Developer!', CURRENT_DATE(), false);
+
+insert into TO_DO_POJO (ID, USER_NAME, DESCRIPTION, DATE, DONE) values(1005, 'Karthik', 'Learn AWS Developer!', CURRENT_DATE(), false);
